880ec9c9d0181a8cfd64d69068dfa26134fdd6aa
Crisp Digital <cdadmin@macs-MacBook-Pro.local>
2023-08-09 00:35:57 +0545

    JavaScript program to compute the sum of the two given integers. If the two values are the same, then return triple their sum.

--- answer/js_answer/challenges.js
+++ answer/js_answer/challenges.js
@@ -2,5 +2,18 @@
 // console.log("hellow world!!!");
 
 // - Write a JavaScript program to set the value of PI.
-const piValue = Math.PI;
-console.log(piValue);
+// const piValue = Math.PI;
+// console.log(piValue);
+
+// 1. Write a JavaScript program to compute the sum of the two given integers. If the two values are the same, then return triple their sum.
+// const val1 = Number(prompt("Enter first number"));
+// const val2 = Number(prompt("Enter second number"));
+// if (val1 === val2) {
+//   const sumNum = val1 + val2;
+//   const tripleNum = sumNum * 3;
+//   alert("The triple sum is " + tripleNum);
+// } else {
+//   alert(
+//     "The given number aren't equal to eacthother please re-enter the same number"
+//   );
+// }
