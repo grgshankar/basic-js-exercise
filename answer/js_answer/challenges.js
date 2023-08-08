7d59f5da0a27659bfc99fe8f205243ff4ef1a88a
Crisp Digital <cdadmin@macs-MacBook-Pro.local>
2023-08-09 00:30:32 +0545

    setting the value of PI

---
+++ answer/js_answer/challenges.js
@@ -0,0 +1,6 @@
+// - Write a JavaScript program that prints 'Hello World'.
+// console.log("hellow world!!!");
+
+// - Write a JavaScript program to set the value of PI.
+const piValue = Math.PI;
+console.log(piValue);
